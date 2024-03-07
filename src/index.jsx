e9c8d4a62263f7ae96b80a2b151441eb5fe7367f
Tetiana Poletaieva <ptupob@gmail.com>
2024-03-07 20:30:16 +0200

    change router

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { CartProvider } from "./components/CartContext";
 
 import App from "./App";
